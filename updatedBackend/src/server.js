e3fd3ea3e3678f2db89068c6d4937b95190c18bf
rahulkumar6777 <rahulk48546@gmail.com>
2026-04-29 13:57:20 +0530

    feat: update rate limiting configuration for API requests

--- updatedBackend/src/server.js
+++ updatedBackend/src/server.js
@@ -15,10 +15,11 @@ import { rateLimit } from 'express-rate-limit'
 app.use(
     "/api",
     rateLimit({
-        windowMs: 15 * 60 * 1000,
-        max: 400,
+        windowMs: 60 * 1000,
+        max: 100,
         standardHeaders: true,
         legacyHeaders: false,
+        message: { error: "Too many requests, please try again later." }
     }),
 );
 
